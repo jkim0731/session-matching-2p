da349bdf88c20ac25e6ffd3222c015d64a86f94a
Jinho Kim <jkimkj@gmail.com>
2020-12-12 23:21:31 -0800

    Update conversion.m
    Add checking errors after the conversion
    - Checking file size match
    - Checking sbx and h5 frames

--- conversion.m
+++ conversion.m
@@ -1,4 +1,6 @@
 clear; close all; 
+%% Basic info
+
 baseDir = 'E:\';
 mouse = 25;
 sessions = [3:5];
@@ -6,7 +8,9 @@ sessions = [3:5];
 elapsedTimes = zeros(length(sessions),1);
 
 targetBD = 'E:\025\';
-%%
+targetDir = sprintf('%s%03d\\',targetBD,mouse);
+
+%% Convert sbx files into h5 files in each imaging plane
 for si = 1 : length(sessions)
     if sessions(si) < 1000
         sbxList = ls(sprintf('%s%03d\\%03d_%03d_0*.sbx',baseDir,mouse,mouse,sessions(si)));
@@ -34,33 +38,145 @@ for si = 1 : length(sessions)
             error('Wrong session #')
         end
 
-        targetDir = sprintf('%s%03d\\',targetBD,mouse);
+        
         if isfile([fn,'.trials'])
             convertToH5_JK(fn,targetDir,optotuneRingingTime,1000)
         end
     end
 end
 
-%% Check frame index
-% baseDir = 'P:\';
-% mouse = 38;
-% sessions = [16:31,5554,5555,9998,9999];
-% maxIdx = zeros(length(sessions),2); %(:,1) from .trials, (:,2) from sbx_maxidx
-% % for si = 1 : length(sessions)
-% for si = 17  
-%     sbxList = ls(sprintf('%s%03d\\%03d_%03d_0*.sbx',baseDir,mouse,mouse,sessions(si)));
+
+
+
+
+
+%% Check results
+%% (1) Test file size. Planes 1-4 from a session should have the same file size, and planes 5-8 should do too.
+%% Sometimes the file sizes are different, meaning there was an error
+
+
+% errorSessions = [];
+% for si = 1 : length(sessions)
+%     session = sessions(si);
+%     if session < 1000
+%         sbxList = ls(sprintf('%s%03d\\%03d_%03d_0*.sbx',baseDir,mouse,mouse,session));
+%         if mouse == 27 % mouse JK027 has only lower planes
+%             planes = 5:8;
+%         else
+%             planes = 1:8;
+%         end
+%     else
+%         sbxList = ls(sprintf('%s%03d\\%03d_%d_*.sbx',baseDir,mouse,mouse,session));
+%         if mod(session,2)
+%             planes = 1:4;
+%         else
+%             planes = 5:8;
+%         end
+%     end
+%     
 %     for sbxi = 1 : size(sbxList,1)
-%         sbxFn = sbxList(sbxi,1:end-4); % removing '.sbx'
-%         fn = sprintf('%s%03d\\%s',baseDir,mouse,sbxFn);
-% %         if sessions(si) > 1000 % spontaneous or piezo sessions
-%             laserOnFrames = laser_on_frames(fn);
-%             jksbxsplittrial(fn,laserOnFrames)
-% %         else
-% %             jksbxsplittrial(fn) % run this again, making .trial files
-% %         end
-%         
-%         trials = importdata([fn,'.trials']);
-%         maxIdx(si,1) = max(cellfun(@max, trials.frame_to_use));
-%         maxIdx(si,2) = sbx_maxidx(fn);
+%         tempFn = strsplit(sbxList(sbxi,:),'.');
+%         sbxFn = tempFn{1}; % removing '.sbx'
+%         % first, check if there are all files matched to each plane
+%         for pi = 1 : length(planes)
+%             if ~isfile(sprintf('%splane_%d\\%s_plane_%d.h5',targetDir,planes(pi),sbxFn,planes(pi)))
+%                 errorSessions = [errorSessions;session];
+%                 break % break pi loop
+%             end
+%         end
+%         if ismember(session, errorSessions)
+%             break
+%         end
+%         % then, check if these files have the same file size
+%         % after dividing into imaging volumes
+%         if ismember(1,planes) % upper volume, if exists
+%             fileSizes = zeros(4,1);
+%             for pi = 1 : 4
+%                 temp = dir(sprintf('%splane_%d\\%s_plane_%d.h5',targetDir,pi,sbxFn,pi));
+%                 fileSizes(pi) = temp.bytes;
+%             end
+%             if any(diff(fileSizes))
+%                 errorSessions = [errorSessions;session];
+%                 break
+%             end
+%         end
+%         if ismember(5,planes) % lower volume, if exists
+%             fileSizes = zeros(4,1);
+%             for pi = 5 : 8
+%                 temp = dir(sprintf('%splane_%d\\%s_plane_%d.h5',targetDir,pi,sbxFn,pi));
+%                 fileSizes(pi-4) = temp.bytes;
+%             end
+%             if any(diff(fileSizes))
+%                 errorSessions = [errorSessions;session];
+%                 break
+%             end
+%         end
 %     end
 % end
+% errorSessions
+
+
+
+%% (2) Check random files and random frames from each sbx file
+
+
+% numCheckFrames = 100;
+% errorSessions = [];
+% for si = 1 : length(sessions)
+%     session = sessions(si);
+%     if session < 1000
+%         sbxList = ls(sprintf('%s%03d\\%03d_%03d_0*.sbx',baseDir,mouse,mouse,session));
+%         if mouse == 27 % mouse JK027 has only lower planes
+%             planes = 5:8;
+%             ftuIndices = 5:8; % ftu: frame_to_use
+%         else
+%             planes = 1:8;
+%             ftuIndices = 1:8; % ftu: frame_to_use
+%         end
+%     else
+%         sbxList = ls(sprintf('%s%03d\\%03d_%d_*.sbx',baseDir,mouse,mouse,session));
+%         if mod(session,2)
+%             planes = 1:4;
+%             ftuIndices = 1:4; % ftu: frame_to_use
+%         else
+%             planes = 5:8;
+%             ftuIndices = 1:4; % ftu: frame_to_use
+%         end
+%     end
+%     
+%     for sbxi = 1 : size(sbxList,1)
+%         tempFn = strsplit(sbxList(sbxi,:),'.');
+%         sbxFn = tempFn{1}; % removing '.sbx'
+%         refFn = sprintf('%s%03d\\%s',baseDir,mouse,sbxFn);
+%         trials = load([refFn,'.trials'],'-mat');
+%         loadedInfo = load([refFn,'.mat'],'info');
+%         
+%         for ftui = 1 : length(ftuIndices)
+%             plane = planes(ftui);
+%             h5Fn = sprintf('%splane_%d\\%s_plane_%d.h5',targetDir, plane, sbxFn, plane);
+%             numFrames = length(trials.frame_to_use{ftuIndices(ftui)});
+%             testFrames = randperm(numFrames,numCheckFrames);
+%             yStart = round(optotuneRingingTime/ (1000/loadedInfo.info.resfreq) *(2-loadedInfo.info.scanmode));
+%             xStart = 100;
+%             xDeadband = 10;
+%             
+%             % Load frames from sbx file (reference images)
+%             refImRaw = squeeze(jksbxreadframes_4h5c(refFn,trials.frame_to_use{ftuIndices(ftui)}(testFrames),1));
+%             refIm = refImRaw(yStart:end,xStart:end-xDeadband,:);
+%             
+%             % Load frames from h5 file (check images)
+%             checkIm = zeros(size(refIm), 'like', refIm);
+%             for fi = 1 : numCheckFrames
+%                 checkIm(:,:,fi) = h5read(h5Fn, '/data', [1 1 testFrames(fi)], [size(refIm,1), size(refIm,2), 1]);
+%             end
+%             
+%             % Compare reference images and check images
+%             imDiff = refIm - checkIm;
+%             if any(imDiff(:))
+%                 errorSessions = [errorSessions; session];
+%                 break
+%             end
+%         end
+%     end
+% end
+% errorSessions
